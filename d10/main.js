75c36bbe002ff30e9c6f49c03156bb4fb9d7ac1d
badjinn <g.sottovia@yahoo.com>
2023-05-26 21:40:34 +0200

    boh

--- d10/main.js
+++ d10/main.js
@@ -1,1 +1,29 @@
 /*Still nothing*/
+
+let primoNumero= 12;
+let numeratore = 30;
+let denumeretore = 2;
+
+let divisione = numeratore/denumeretore;
+
+console.log("valore di divisione: ", divisione);
+
+console.log(typeof divisione);
+
+const str = `Riultato della divisione: ${divisione}.`;
+console.log(str);
+
+//&&
+//||
+//!
+
+/*
+
+=   Assegnazione
+==  equality (due espressioni hanno lo stesso valore? con conversioni)      2 == "2" TRUE
+=== Identity (due espressioni hanno lo stesso valore? senza conversioni)    2 == "2" FALSE
+
+
+
+
+*/
