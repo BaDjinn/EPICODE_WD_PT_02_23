b51dfa40fe938f2ee68ce9d29375e08594cfefae
BaDjinn <wolf_arena@yahoo.it>
2023-07-20 19:17:05 +0200

    .

--- M3.d8/main.js
+++ M3.d8/main.js
@@ -1,8 +1,18 @@
 function toggleMenu(){
 const menu = document.getElementById("menu");
 menu.classList.toggle('menu-open')
+
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+hamburgerMenu.classlist.toggle('open')
 };
 
 window.addEventListener("DOMContentLoaded", function(evt){
     const menuBtn = document.getElementById("menuBtn")
+
+    menuBtn.addEventListener("click", function(){
+        toggleMenu();
+    })
 })
+
+
+
