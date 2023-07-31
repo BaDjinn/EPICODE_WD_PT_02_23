4315328ac6e7bc62108793d9583c426594da8542
BaDjinn <wolf_arena@yahoo.it>
2023-07-31 19:46:30 +0200

    .

--- M4_02/main.js
+++ M4_02/main.js
@@ -33,43 +33,83 @@
 // xhr.send();
 
 
-const makeAjaxCall = (url, callback) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onload = () => {
-        if (xhr.status === 200) {
-            callback(null, xhr.response);
-        } else {
-            callback('Errore durante la richiesta');
-        }
-    };
-    xhr.onerror = () => { callback('Errore di rete'); };
-    xhr.send();
-};
-
-function getUserDetails(userId, clbk) {
-    makeAjaxCall(`https://jsonplaceholder.typicode.com/users/${userId}`, clbk);
-}
-
-function getUserPost(userId, clbk) {
-    makeAjaxCall(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, clbk);
-}
-
-console.log('Inizio del programma');
-
-getUserDetails(1, function (err, userDetails) {
-    if (err) console.log(err);
-    else {
-        const user = JSON.parse(userDetails);
-        getUserPost(user.id, function (err, userPosts) {
-            if (err) console.log('Errore', err);
-            else {
-                const allUserPosts = JSON.parse(userPosts);
-                console.log('Post dell\'utente: ', allUserPosts);
+/*
+    const makeAjaxCall = (url, callback) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                callback(null, xhr.response);
+            } else {
+                callback('Errore durante la richiesta');
             }
-            console.log('FINE');
-        });
+        };
+        xhr.onerror = () => { callback('Errore di rete'); };
+        xhr.send();
+    };
+    
+    function getUserDetails(userId, clbk) {
+        makeAjaxCall(`https://jsonplaceholder.typicode.com/users/${userId}`, clbk);
     }
+    
+    function getUserPost(userId, clbk) {
+        makeAjaxCall(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`, clbk);
+    }
+    
+    console.log('Inizio del programma');
+    
+    getUserDetails(1, function (err, userDetails) {
+        if (err) console.log(err);
+        else {
+            const user = JSON.parse(userDetails);
+            getUserPost(user.id, function (err, userPosts) {
+                if (err) console.log('Errore', err);
+                else {
+                    const allUserPosts = JSON.parse(userPosts);
+                    console.log('Post dell\'utente: ', allUserPosts);
+                }
+                console.log('FINE');
+            });
+        }
+    });
+    
+    console.log('FINE PROGRAMMA');
+*/
+/*--------------------------PROMISES--------------*/
+
+const My_promise = new Promise(function(res,rej){
+
+    setTimeout(()=>{
+        const randomValue = Math.random();
+        if (randomValue>0.5) res(randomValue);
+        else rej(new Error('Valore troppo basso'));
+    },1000)
 });
 
-console.log('FINE PROGRAMMA');
+my_promise
+.then(res=> console.log('res: ',res))
+.catch(err=> console.log('err: ', err))
+
+
+/*------------------------------MULTIPLE PROMISES---------------- */
+
+const makeRequest=(url2) => {
+    return new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url2);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(xhr.response);
+            } else {
+                reject(new Error('Errore durante la richiesta'));
+            }
+        };
+        xhr.onerror = () => { reject(new Error('Errore di rete')); };
+        xhr.send();
+    })
+}
+makeRequest('https://jsonplaceholder.typicode.com/comments').then(
+    (response)=>{
+        const res1 = JSON.parse(response)
+    }
+).catch()
