3c41d62dcbe877567c0c1946b4eecec1c07ef320
BaDjinn <g.sottovia@yahoo.com>
2023-08-31 19:34:35 +0200

    .

---
+++ M5_02/p2/data/navLinks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class navLinks extends Component {
+  render() {
+    return (
+      <div>navLinks</div>
+    )
+  }
+}
+
+export default navLinks
